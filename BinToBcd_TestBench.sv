a06dca25c70878c3c6bb807f8b144d25e7325cba
ChUrl <christoph.urlacher@protonmail.com>
2023-03-27 23:29:16 +0200

    Reformat testbenches

--- BinToBcd_TestBench.sv
+++ BinToBcd_TestBench.sv
@@ -16,42 +16,42 @@ module BinToBcd_TestBench;
   
   BinToBcd bcd(
     .binin(binout),
-	 .bcdout(value)
+    .bcdout(value)
   );
   
 // synthesis translate_off
   integer ii;
   initial begin
     $timeformat(-9, 2, " ns", 20);
-	 
+    
     $display("%0t Reset", $time);
-	 decrement = 0;
+    decrement = 0;
     #20 reset = 1;
-	 #20 reset = 0;
-	 assert (value[0] == 0);
-	 assert (value[1] == 0);
-	 assert (value[2] == 0);
-	 assert (value[3] == 0);
-	 
-	 $display("%0t Bin[1] = 0 0 0 1", $time);
-	 #20 clock = 1;
-	 #20 clock = 0;
-	 assert (value[0] == 1);
-	 assert (value[1] == 0);
-	 assert (value[2] == 0);
-	 assert (value[3] == 0);
-	 
-	 $display("%0t Bin[1024] = 1 0 2 4", $time);
-	 for (ii = 0; ii < 1023; ii = ii + 1) begin
-	   #20 clock = 1;
-		#20 clock = 0;
-	 end
-	 assert (value[0] == 4);
-	 assert (value[1] == 2);
-	 assert (value[2] == 0);
-	 assert (value[3] == 1);
-	 
-	 $display("Success!");
+    #20 reset = 0;
+    assert (value[0] == 0);
+    assert (value[1] == 0);
+    assert (value[2] == 0);
+    assert (value[3] == 0);
+   
+    $display("%0t Bin[1] = 0 0 0 1", $time);
+    #20 clock = 1;
+    #20 clock = 0;
+    assert (value[0] == 1);
+    assert (value[1] == 0);
+    assert (value[2] == 0);
+    assert (value[3] == 0);
+   
+   $display("%0t Bin[1024] = 1 0 2 4", $time);
+   for (ii = 0; ii < 1023; ii = ii + 1) begin
+     #20 clock = 1;
+     #20 clock = 0;
+   end
+   assert (value[0] == 4);
+   assert (value[1] == 2);
+   assert (value[2] == 0);
+   assert (value[3] == 1);
+   
+   $display("Success!");
   end
   
 // synthesis translate_on
--- Counter_TestBench.sv
+++ Counter_TestBench.sv
@@ -16,61 +16,61 @@ module Counter_TestBench;
   integer ii;
   initial begin
     $timeformat(-9, 2, " ns", 20);
-	 
-	 $display("%0t Initial Reset", $time);
+    
+    $display("%0t Initial Reset", $time);
     #20 reset = 1;
-	 #20 reset = 0;
-	 assert (value == 0);
-	 
-	 $display("%0t Increment 1024x", $time);
-	 decrement = 0;
-	 for (ii = 0; ii < 1024; ii = ii + 1) begin
-		#20 clock = 1;
-		#20 clock = 0;
-	   assert (value == ii + 1);
-	 end
-	 
-	 $display("%0t Decrement 1024x", $time);
-	 decrement = 1;
-	 for (ii = 1024; ii > 0; ii = ii - 1) begin
-		#20 clock = 1;
-		#20 clock = 0;
-	   assert (value == ii - 1);
-	 end
-	 
-	 $display("%0t Increment 1024x", $time);
-	 decrement = 0;
-	 for (ii = 0; ii < 1024; ii = ii + 1) begin
-		#20 clock = 1;
-		#20 clock = 0;
-	   assert (value == ii + 1);
-	 end
-	 
-	 $display("%0t Reset", $time);
-	 #20 reset = 1;
-	 #20 reset = 0;
-	 assert (value == 0);
-	 
-	 $display("%0t Increment 9999x", $time);
-	 decrement = 0;
-	 for (ii = 0; ii < 9999; ii = ii + 1) begin
-		#20 clock = 1;
-		#20 clock = 0;
-	   assert (value == ii + 1);
-	 end
-	 
-	 $display("%0t Overflow", $time);
-	 #20 clock = 1;
-	 #20 clock = 0;
-	 assert (value == 0);
-	 
-	 $display("%0t Underflow", $time);
-	 decrement = 1;
-	 #20 clock = 1;
-	 #20 clock = 0;
-	 assert (value == 9999);
-	 
-	 $display("Success!");
+    #20 reset = 0;
+    assert (value == 0);
+    
+    $display("%0t Increment 1024x", $time);
+    decrement = 0;
+    for (ii = 0; ii < 1024; ii = ii + 1) begin
+      #20 clock = 1;
+      #20 clock = 0;
+      assert (value == ii + 1);
+    end
+    
+    $display("%0t Decrement 1024x", $time);
+    decrement = 1;
+    for (ii = 1024; ii > 0; ii = ii - 1) begin
+      #20 clock = 1;
+      #20 clock = 0;
+      assert (value == ii - 1);
+    end
+    
+    $display("%0t Increment 1024x", $time);
+    decrement = 0;
+    for (ii = 0; ii < 1024; ii = ii + 1) begin
+      #20 clock = 1;
+      #20 clock = 0;
+      assert (value == ii + 1);
+    end
+    
+    $display("%0t Reset", $time);
+    #20 reset = 1;
+    #20 reset = 0;
+    assert (value == 0);
+    
+    $display("%0t Increment 9999x", $time);
+    decrement = 0;
+    for (ii = 0; ii < 9999; ii = ii + 1) begin
+      #20 clock = 1;
+      #20 clock = 0;
+      assert (value == ii + 1);
+    end
+    
+    $display("%0t Overflow", $time);
+    #20 clock = 1;
+    #20 clock = 0;
+    assert (value == 0);
+    
+    $display("%0t Underflow", $time);
+    decrement = 1;
+    #20 clock = 1;
+    #20 clock = 0;
+    assert (value == 9999);
+    
+    $display("Success!");
   end
   
 // synthesis translate_on
